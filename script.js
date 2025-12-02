f67182979ab05cbc51fe8e28bad9df33100a15d9
test <koush0811@gmail.com>
2025-12-02 17:59:42 +0900

    B4完成

---
+++ script.js
@@ -0,0 +1,6 @@
+function Check(){
+    document.getElementById('list1').style.textDecoration  = 'line-through';
+}
+function Delete(){
+    document.getElementById('list1').style.textDecoration  = 'none';
+}
