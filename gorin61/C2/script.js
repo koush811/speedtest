2f83c67ddf54fd6b87e4fbc7c030e1132ec34d0b
koush811 <koush0811@gmail.com>
2025-12-07 17:58:28 +0900

    C2コード修正

--- gorin61/C2/script.js
+++ gorin61/C2/script.js
@@ -1,16 +1,19 @@
 const img = document.getElementById('img')
+const mouse = document.getElementById('hilight')
 let size = 150;
 let isActive = false;
+let x = 0;
+let y = 0;
 
 img.addEventListener('mousemove',(e)=>{
     const rect = img.getBoundingClientRect();
-    const x = Math.floor((e.clientX - rect.left)/10)*10;
-    const y = Math.floor((e.clientY - rect.left)/10)*10;
+    console.log('e.clientX')
+    x = Math.floor((e.clientX - rect.left)/10)*10;
+    y = Math.floor((e.clientY - rect.left)/10)*10;
     console.log(x,y)
-    const mouse = document.getElementById('hilight')
-    mouse.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.75) 150px)`;
-    
+    mouse.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.75) ${size}px)`;
 })
+
 
 img.addEventListener("mouseenter", () => {
   isActive = true;
@@ -23,11 +26,11 @@ img.addEventListener("mouseleave", () => {
 });
 
 
-
-document.activeElement('wheel',function(e){
-    if (!isActive) return;
-    const delta = Math.sign(e.deltaY); 
-    size += delta * 10;
-    size = Math.max(10, size);
-    mouse.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.75) ${size}px)`;
+img.addEventListener('wheel', function(e){
+      if (!isActive) return;
+      e.preventDefault();
+      const delta = Math.sign(e.deltaY); 
+      size += delta * 10;
+      size = Math.max(10, size);
+      mouse.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.75) ${size}px)`;
 })
