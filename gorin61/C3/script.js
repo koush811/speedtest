cdb1f92a242c48044e7c8ed8fc778c16ceb9f810
test <koush0811@gmail.com>
2025-12-08 22:55:07 +0900

    C3

--- gorin61/C3/script.js
+++ gorin61/C3/script.js
@@ -1,7 +1,34 @@
-const vr = document.getElementById('value-r').value
-const vg = document.getElementById('value-g').value
-const vb = document.getElementById('value-b').value
-console.log(vr,vg,vb)
+const input = document.getElementById('input')
+const color = document.getElementById('color')
 
+input.addEventListener('input',()=>{
+    const value = input.value.trim()
+    const hex =  /^#([0-9a-fA-F]{6})$/;
+    const rgb =   /^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/;
 
-document.getElementById('color').style.backgroundColor = `rgba(${vr},${vg},${vb},1)`
+    if(hex.test(value)){
+        let hexv = value.match(hex)[1];
+        let r = parseInt(hexv.substring(0, 2), 16);
+        let g = parseInt(hexv.substring(2, 4), 16);
+        let b = parseInt(hexv.substring(4, 6), 16);
+        color.textContent = `RGB: rgb(${r}, ${g}, ${b})`
+        color.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    }else if(rgb.test(value)){
+        let [,r,g,b] = value.match(rgb)
+        r = Number(r);
+        g = Number(g);
+        b = Number(b);
+        if ([r, g, b].some(x => x < 0 || x > 255)) {
+        color.textContent = 'エラー';
+        return;
+        }
+        let hexv = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+        color.textContent = `HEX: ${hexv}`;
+        color.style.backgroundColor = `${hexv}`
+    } else {
+        color.textContent = 'エラー';
+        
+    }
+    
+
+})
